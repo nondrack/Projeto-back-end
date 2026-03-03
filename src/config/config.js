9b3bd44df1fca2bc3e73ffc0ee2ebe2563a7da1d
nondrack <luckasfernando675@gmail.com>
2026-03-02 21:41:26 -0300

    arrumando

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  development: {
-    username: 'root',
-    password: '1234',
-    database: 'filmes_2026',
-    host: 'localhost',
-    dialect: 'mysql', // ou mysql
-  },
-};
--- src/config/index.ts
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from "../models";
-
-const sequelize = new Sequelize(
-    "filmes_2026",
-    "root",
-    
-)
